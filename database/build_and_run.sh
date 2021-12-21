6423358861a6afa1417cf72e4c2186031c0aacc6
thiswind <thiswind@gmail.com>
2021-12-21 07:23:33 +0000

    完成了基于SQL的数据保存

--- database/build_and_run.sh
+++ database/build_and_run.sh
@@ -11,6 +11,9 @@ MY_MARIADB_ROOT_PASSWORD="ldkajiejdkakjhtvcy6882h2ny"
 MY_HOST="127.0.0.1"
 MY_PORT="3306"
 
+MY_DB_NAME="markexp"
+MY_DB_TABLE_NAME="conversation"
+
 
 # docker build -t ${IMAGE} .
 
@@ -19,31 +22,40 @@ docker container rm ${CONTAINER}
 
 # docker run --name ${CONTAINER} -p 3306:3306 -d ${IMAGE}
 
-docker run --detach --name ${CONTAINER} --env MARIADB_USER=${MY_MARIADB_USER} --env MARIADB_PASSWORD=${MY_MARIADB_PASSWORD} --env MARIADB_ROOT_PASSWORD=${MY_MARIADB_ROOT_PASSWORD} -p 3306:3306  mariadb:latest
+docker run \
+   --detach \
+   --name ${CONTAINER} \
+   --env MARIADB_USER=${MY_MARIADB_USER} \
+   --env MARIADB_PASSWORD=${MY_MARIADB_PASSWORD} \
+   --env MARIADB_ROOT_PASSWORD=${MY_MARIADB_ROOT_PASSWORD} \
+   -p 3306:3306  \
+   mariadb:latest
 
 # docker exec -ti ${CONTAINER} /bin/bash
 
 
-export MY_MARIADB_USER="markexp"
-export MY_MARIADB_PASSWORD="laksdjfienilakdjlkajdsfaljeilie"
-export MY_MARIADB_ROOT_PASSWORD="ldkajiejdkakjhtvcy6882h2ny"
-export MY_HOST="127.0.0.1"
-export MY_PORT="3306"
+# MY_MARIADB_USER="markexp"
+# MY_MARIADB_PASSWORD="laksdjfienilakdjlkajdsfaljeilie"
+# MY_MARIADB_ROOT_PASSWORD="ldkajiejdkakjhtvcy6882h2ny"
+# MY_HOST="127.0.0.1"
+# MY_PORT="3306"
 
 sleep 7
 
 echo "mysql -u root -h ${MY_HOST} -P ${MY_PORT} -p${MY_MARIADB_ROOT_PASSWORD} -e \"show databases;\""
 
 mysql -u root -h ${MY_HOST} -P ${MY_PORT} -p${MY_MARIADB_ROOT_PASSWORD} -e "
-create database markexp;
-grant select,insert,update,delete on markexp.* to markexp@\"%\" identified by \"markexp\";
-use markexp;
-CREATE TABLE IF NOT EXISTS conversation(
+create database ${MY_DB_NAME};
+use ${MY_DB_NAME};
+
+CREATE TABLE IF NOT EXISTS ${MY_DB_TABLE_NAME}(
    msg_id INT UNSIGNED AUTO_INCREMENT,
    msg VARCHAR(100) NOT NULL,
    PRIMARY KEY (msg_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+INSERT INTO ${MY_DB_TABLE_NAME} (msg) VALUES ('请开始你的表演');
+GRANT ALL PRIVILEGES ON ${MY_DB_NAME}.* TO '${MY_MARIADB_USER}'@'%' IDENTIFIED BY '${MY_MARIADB_PASSWORD}';
+select * from ${MY_DB_TABLE_NAME};
 show databases;
 "
 
--- midware/expapp.py
+++ midware/expapp.py
@@ -2,15 +2,54 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pymysql
+
+
+MY_MARIADB_USER = "markexp"
+MY_MARIADB_PASSWORD = "laksdjfienilakdjlkajdsfaljeilie"
+MY_MARIADB_ROOT_PASSWORD = "ldkajiejdkakjhtvcy6882h2ny"
+MY_HOST = "127.0.0.1"
+MY_PORT = "3306"
+
+MY_DB_NAME = "markexp"
+MY_DB_TABLE_NAME = "conversation"
+
 message_list = []
 
 
 def process_input(msg):
-    message_list.append(msg)
+    # message_list.append(msg)
+
+    db = pymysql.connect(host=MY_HOST,
+                         user=MY_MARIADB_USER,
+                         password=MY_MARIADB_PASSWORD,
+                         database=MY_DB_NAME)
+
+    cursor = db.cursor()
+    cursor.execute(f'INSERT INTO {MY_DB_TABLE_NAME} (msg) VALUES ("{msg}")')
+    db.commit()
+    db.close()
+
     pass
 
 
 def get_output():
+
+    db = pymysql.connect(host=MY_HOST,
+                         user=MY_MARIADB_USER,
+                         password=MY_MARIADB_PASSWORD,
+                         database=MY_DB_NAME)
+
+    cursor = db.cursor()
+    cursor.execute(f'select * from {MY_DB_TABLE_NAME}')
+    results = cursor.fetchall()
+
+    message_list.clear()
+
+    for one_message in results:
+        msg = one_message[1]
+        message_list.append(msg)
+
     return message_list
 
 
@@ -19,7 +58,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('conversation.jinja2')
+    conversation = get_output()
+
+    return render_template('conversation.jinja2', conversation=conversation)
 
 
 @app.route("/input", methods=['GET', 'PoSt'])
