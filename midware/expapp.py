c9dad4f7fe269678813a88708959ff0a9ba2f5df
thiswind <thiswind@gmail.com>
2021-12-28 08:03:42 +0000

    update: 在python当中创建数据库的表

--- midware/expapp.py
+++ midware/expapp.py
@@ -7,29 +7,65 @@ import os
 
 CONTAINER = "markexp-mariadb"
 
-MY_MARIADB_USER = "markexp"
-MY_MARIADB_PASSWORD = "laksdjfienilakdjlkajdsfaljeilie"
-MY_MARIADB_ROOT_PASSWORD = "ldkajiejdkakjhtvcy6882h2ny"
+MARIADB_USER = "velcom"
+MARIADB_PASSWORD = "velcom_alksdjflkakjei"
+MY_MARIADB_ROOT_PASSWORD = "velcom_rootlkdjalienfklae"
 MY_HOST = "127.0.0.1"
 MY_PORT = "3306"
 
-MY_DB_NAME = "markexp"
+MY_DB_NAME = "velcom"
 MY_DB_TABLE_NAME = "conversation"
 
 # 从环境变量当中读取 `MY_HOST`, 如果存在这个环境变量，那么就覆盖本地的 `MY_HOST`
 my_host_hame = os.getenv('MY_HOST')
 if my_host_hame:
     MY_HOST = my_host_hame
+    print('############################################')
+    print(f'get MY_HOST from env: {MY_HOST}')
+    print('############################################')
+
 
 message_list = []
+
+
+first_time = True
+
+
+def create_table():
+    print('############################################')
+    print(f'creating table {MY_DB_TABLE_NAME}')
+    print('############################################')
+
+    global first_time
+
+    if first_time:
+        db = pymysql.connect(host=MY_HOST,
+                             user='root',
+                             password=MY_MARIADB_ROOT_PASSWORD,
+                             database=MY_DB_NAME)
+        cursor = db.cursor()
+        cursor.execute('use velcom')
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS conversation(msg_id INT UNSIGNED AUTO_INCREMENT,msg VARCHAR(100) NOT NULL,PRIMARY KEY (msg_id))ENGINE=InnoDB DEFAULT CHARSET=utf8;')
+        cursor.execute('INSERT INTO conversation (msg) VALUES ("请开始你的表演")')
+        db.commit()
+        db.close()
+
+        first_time = False
+
+        print('############################################')
+        print(f'table {MY_DB_TABLE_NAME} created')
+        print('############################################')
+    else:
+        pass
 
 
 def process_input(msg):
     # message_list.append(msg)
 
     db = pymysql.connect(host=MY_HOST,
-                         user=MY_MARIADB_USER,
-                         password=MY_MARIADB_PASSWORD,
+                         user='root',
+                         password=MY_MARIADB_ROOT_PASSWORD,
                          database=MY_DB_NAME)
 
     cursor = db.cursor()
@@ -43,8 +79,8 @@ def process_input(msg):
 def get_output():
 
     db = pymysql.connect(host=MY_HOST,
-                         user=MY_MARIADB_USER,
-                         password=MY_MARIADB_PASSWORD,
+                         user='root',
+                         password=MY_MARIADB_ROOT_PASSWORD,
                          database=MY_DB_NAME)
 
     cursor = db.cursor()
@@ -81,13 +117,9 @@ def input():
     return render_template('conversation.jinja2', conversation=conversation)
 
 
+# init
+create_table()
+
+
 if __name__ == '__main__':
     app.run()
-
-# MY_MARIADB_USER="markexp"
-# MY_MARIADB_PASSWORD="laksdjfienilakdjlkajdsfaljeilie"
-# MY_MARIADB_ROOT_PASSWORD="ldkajiejdkakjhtvcy6882h2ny"
-# MY_HOST="127.0.0.1"
-# MY_PORT="3306"
-
-# MY_DATABASE="markexp"
